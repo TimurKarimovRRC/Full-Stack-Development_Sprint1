f7687ed4133623e072ebb468b865e7fbff038640
Timur Karimov <tkarimov@academic.rrc.ca>
2026-09-09 15:06:36 -0500

    Add Advetures component and remove some garbage form vite install

---
+++ src/component/Adventurers.tsx
@@ -0,0 +1,19 @@
+import adventurerData from "../data/adventurers.json";
+
+export function Adventurers() {
+    const adventurers = adventurerData;
+
+    return (
+        <section className="adventurers">
+            <h2>Adventurers</h2>
+
+            <ul>
+                {adventurers.map((adventurer) => (
+                    <li key={adventurer.id}>
+                        {adventurer.name} — {adventurer.characterClass}
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+}
