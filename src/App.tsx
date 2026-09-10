831be8d9f9e01df903712662dcc4505439517ef5
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-09-10 14:40:55 -0500

    feat: added mock data for quest assignment component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { Adventurers } from "./component/Adventurers";
-import { QuestAssignments } from "./component/quest-assignments/QuestAssignment";
 import "./App.css";
 
 export function App() {
-    return (
-        <>
-            <header className="site-header parchment">
-                <h1>Fantasy Quest Board</h1>
-            </header>
-
-            <main className="parchment">
+    const members = [
+        "Timur Karimov",
+        "Aum Mistry",
+        "MacKinley Wanless"
+    ];
+        return (
+            <Layout title="Fantasy Quest Board" members={members}>
                 <Adventurers />
-                <QuestAssignments />
             </main>
 
             <footer className="site-footer parchment">
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,8 @@
+export function Nav() {
+    return (
+       
+        <nav className="site-nav parchment" aria-label="Main navigation">
+            <a href="#main-content">Adventurers</a>
+        </nav>
+    );
+}
